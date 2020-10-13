0caa24a054f18aecff13d8e79f8c48f1d99db5c4
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-13 15:58:29 +0200

    Add project files.

---
+++ FlowControl Oefening2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl_Oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Geef een getal in:");
+            int j = Int32.Parse(Console.ReadLine());
+            int k = 0;
+            for (int i = 0; i <= j; i++)
+            {
+                Console.WriteLine($"de getallen zijn: {i} ");
+                k = k + i;
+                Console.WriteLine($"de som is: {k}");
+            }
+        }
+    }
+}
